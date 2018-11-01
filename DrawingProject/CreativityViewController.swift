6a164ab8f9c373bcfaf49408e70ca49d459f7a36
Skylar <skylarhimeno@gmail.com>
2018-11-01 08:08:35 -0600

    Creating a drawing app through xcode

---
+++ DrawingProject/CreativityViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DrawingProject
+//
+//  Created by Himeno, Skylar on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class CreativityViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    public override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning() -> Void
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
